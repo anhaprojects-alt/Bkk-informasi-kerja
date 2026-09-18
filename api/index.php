74d69a059d7a8d5601eb39dbb88bf488829a4079
atifnaufal <atifnaufalh@gmail.com>
2026-09-18 19:18:36 +0700

    "fix: update serverless entry point handler for laravel core kernel"

--- api/index.php
+++ api/index.php
@@ -1,12 +1,18 @@
 <?php
 
-// Jembatan Serverless untuk Vercel agar dapat membaca root path dengan benar
+// 1. Muat autoload Vendor Composer agar semua class Laravel terdeteksi di Vercel
+require __DIR__ . '/../vendor/autoload.php';
+
+// 2. Inisialisasi Aplikasi Laravel Core Bootstrap
 $app = require __DIR__ . '/../bootstrap/app.php';
 
+// 3. Tangani Request Secara Serverless menggunakan Http Kernel Native
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
 $response = $kernel->handle(
     $request = Illuminate\Http\Request::capture()
-)->send();
+);
+
+$response->send();
 
 $kernel->terminate($request, $response);
